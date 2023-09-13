c61aa6dedf37339472147e26e24136505e4d1020
UmbralRevenant <68273777+UmbralRevenant@users.noreply.github.com>
2023-09-13 20:09:47 +0800

    Init Comp3111LEx

--- src/main/java/Lab1/myLibrary.java
+++ src/main/java/Lab1/myLibrary.java
@@ -1,10 +1,10 @@
 package Lab1;
 public class myLibrary {
-    public static int Power(int Base, int Exponent){
+    public static int power(int Base, int Exponent){
         if (Exponent == 1)
             return Base * Exponent;
         else
-            return Base * Power(Base, Exponent-1);
+            return Base * power(Base, Exponent-1);
     }
 
     public static int factorial(int n){
