9d88e78be59526f2ea69a7c6079583b3b8804101
UmbralRevenant <68273777+UmbralRevenant@users.noreply.github.com>
2023-09-20 16:37:21 +0800

    Lab 2 content

---
+++ src/main/java/Lab2c/mainApp2c.java
@@ -0,0 +1,30 @@
+package Lab2c;
+
+//package Lab2b;
+import Lab2b.Book;
+
+import java.util.Arrays;
+
+public class mainApp2c {
+    public static void main(String arg[]){
+        final String array[] = {"Basic Java", "Advance Java", "Guru Java"};
+        Book b = new Book(array);
+        int k = 2;
+        System.out.println("The title of Chapter "+k+" is "+b.getChapter(k-1));
+        String anotherArray[] = b.getChapters();
+
+        System.out.println("There are "+anotherArray.length+" chapters.");
+        // System.out.println(anotherArray);
+        System.out.println(java.util.Arrays.toString(anotherArray));
+
+        // for Exercise 3, add following code:
+        Computer a = new MobileComputer();
+        for (int i = 0; i < 10; i++) a.work();
+
+        Charger c = new Charger();
+        Phone p = new Phone();
+        MobileComputer m = new MobileComputer();
+        c.charge(p);
+        c.charge(m);
+    }
+}
